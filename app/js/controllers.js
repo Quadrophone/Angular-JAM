0530236d5d5d64e3c846f08dfe6067f484b97177
Quadrophone <james@quadrophone.com>
2013-08-16 16:33:06 -0700

    Can now search by specific field

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,8 @@
     function EpisodeControl($scope, $http) {
     $http.get('episodes/JAM.json').success(function(data) {
     $scope.episodes = data;
+    var episodes = {guest: guest, host:host, id:i};
+        EpisodeControl.push(episodes);
     });
         
      
